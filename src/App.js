1fecfe14a5164ab2d408a56840c827bafecd7007
dsausen <53322492+dsausen@users.noreply.github.com>
2019-08-07 21:39:28 -0300

    delete stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Work in progress.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/dsausen"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get woke
-        </a>
-      </header>
-    </div>
+    <h1>Nice</h1>
   );
 }
 
