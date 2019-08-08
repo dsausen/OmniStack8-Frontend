dec5a8933c302af0e08a7233c2814f8093019698
dsausen <53322492+dsausen@users.noreply.github.com>
2019-08-07 21:32:31 -0300

    Initial changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in progress.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/dsausen"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get woke
         </a>
       </header>
     </div>
