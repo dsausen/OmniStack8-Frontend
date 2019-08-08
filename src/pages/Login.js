5995185599c1eab93747f775df8818d8b8a76944
dsausen <53322492+dsausen@users.noreply.github.com>
2019-08-07 23:55:41 -0300

    Make changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Login.css"
 import logo from "../assets/logo.svg";
+import logoReact from "../assets/logoReact.svg";
 
 export default function Login() {
     return (
         <div className="login-container">
             <form>
+                <img src={ logoReact } className="react-logo" alt="React logo"/>
                 <img src={ logo } alt="Tindev logo" />
                 <input placeholder="Digite seu usuário do GitHub" />
                 <button type="submit">Enviar</button>
