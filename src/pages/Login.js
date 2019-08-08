16b27a4c44e23499e16939b9b20fa760a3e1ec2c
dsausen <53322492+dsausen@users.noreply.github.com>
2019-08-07 22:06:51 -0300

    Make changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,12 @@ import logo from "../assets/logo.svg";
 
 export default function Login() {
     return (
-        <img src={ logo } alt="Tindev logo" />
+        <div className="App">
+            <header className="App-header">
+                <img src={ logo } className="App-logo" alt="Tindev logo" />
+                <p>Work in progress. Stay tuned.</p>
+                <a className="App-link" href="https://github.com/dsausen" target="_blank" rel="noopener noreferrer">Get woke</a>
+            </header>
+        </div>
     );
 }
